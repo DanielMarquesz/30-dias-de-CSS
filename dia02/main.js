f7fe2a36db96acbf86f6bf6f6bbd44d240b9b582
Daniel Marques <daniel.marques142@gmail.com>
2020-11-27 23:08:08 -0300

    :tada: dia 2

---
+++ dia02/main.js
@@ -0,0 +1,16 @@
+
+function toggleDown(){    
+    
+    var teste;
+
+    teste = document.getElementById('muda').classList.toggle('show');
+    document.getElementById('nbt').innerHTML = 'Fechar';    
+
+    if (teste === false){
+        document.getElementById('nbt').innerHTML = 'Abrir';
+    }    
+    
+}
+
+
+
