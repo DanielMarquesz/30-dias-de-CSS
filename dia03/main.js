e453f2d4e4c438c6d66cf891fb9fac291c4a3ae9
Daniel Marques <daniel.marques142@gmail.com>
2020-11-29 23:33:58 -0300

    :turtle: Corrigindo estilizações

--- dia03/main.js
+++ dia03/main.js
@@ -1,1 +1,7 @@
 
+function toggleMenu(){   
+
+    document.getElementById('change-menu').classList.toggle('container-show');
+    document.getElementById('change-hamburguer').classList.toggle('hamburguer-hide');     
+    
+}
